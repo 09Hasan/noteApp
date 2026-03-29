1d023030cb8ad95fb0f3b2463071b02094b14570
09Hasan <hasn55667788jmal@gmail.com>
2026-03-29 14:49:37 +0300

    init hive  and open notes box

---
+++ lib/Cubits/constants.dart
@@ -0,0 +1,1 @@
+const String kNotesBoxName = 'notes_box';
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:note_app/Cubits/constants.dart';
 import 'package:note_app/views/home_view.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+  await Hive.openBox(kNotesBoxName);
+
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(brightness: Brightness.dark, fontFamily: 'Poppins'),
-
       home: HomeView(),
     ),
   );
