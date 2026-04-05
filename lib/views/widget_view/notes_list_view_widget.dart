1975c94fb4834d67c5d4d7c3bc9ecc575c1188ee
09Hasan <hasn55667788jmal@gmail.com>
2026-04-05 12:24:26 +0300

    no thing

--- lib/views/widget_view/notes_list_view_widget.dart
+++ lib/views/widget_view/notes_list_view_widget.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:note_app/Cubits/notes_list_cubit/notes_list_cubit.dart';
 import 'package:note_app/models/note_model.dart';
 import 'package:note_app/views/widget_view/note_item.dart';
